fe38d418c19a74a0ec522a5e2be9fa842f34a07f
Леонид Хабибуллин <leonidhabibullin@MacBook-Air.local>
2020-04-13 19:30:13 +0300

    Добавленны методы и селекторы для страницы продукта

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,13 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_FORM_EMAIL = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_FORM_PASS = (By.CSS_SELECTOR, "#id_login-password")
+    REGISTER_FORM_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTER_FORM_PASS = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTER_FORM_PASS_CONFIRM = (By.CSS_SELECTOR, "#id_registration-password2")
+
+class ProductPageLocators():
+    BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
